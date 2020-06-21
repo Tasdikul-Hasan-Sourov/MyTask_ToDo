07cfe9c27a8a7abe23df0fb3addb99319b5b081e
Sourov <thasan1020@gmail.com>
2020-06-21 22:19:53 +0600

    new activity done

--- app/src/main/java/com/example/mytask/Addtask.java
+++ app/src/main/java/com/example/mytask/Addtask.java
@@ -4,8 +4,15 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Addtask extends AppCompatActivity {
+    TextView addTitle,addDes,addtarget;
+    EditText titleDes,desEdit,timeEdit;
+    Button createTask, cancelTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +20,20 @@ public class Addtask extends AppCompatActivity {
         setContentView(R.layout.activity_addtask);
         ActionBar actionBar= getSupportActionBar();
         actionBar.hide();
+        addTitle=(TextView) findViewById(R.id.titleText);
+        titleDes=(EditText) findViewById(R.id.desT);
+        addDes=(TextView) findViewById(R.id.desDes);
+        desEdit=(EditText) findViewById(R.id.desEdit);
+        addtarget=(TextView) findViewById(R.id.timeline);
+        timeEdit=(EditText) findViewById(R.id.timeEdit);
+        createTask=(Button) findViewById(R.id.create);
+        cancelTask=(Button) findViewById(R.id.cancel);
+
+        createTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }

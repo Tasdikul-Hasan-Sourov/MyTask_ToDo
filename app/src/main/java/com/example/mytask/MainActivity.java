49a050b84a7baf6c4c9db1c06ca14379725ff66b
Sourov <thasan1020@gmail.com>
2020-06-21 11:50:34 +0600

    UI Upgrade

--- app/src/main/java/com/example/mytask/MainActivity.java
+++ app/src/main/java/com/example/mytask/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.mytask;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView pageTitle,subTitle,endLine;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        pageTitle=(TextView)findViewById(R.id.pageTitle);
+        subTitle=(TextView) findViewById(R.id.subTitle);
+        endLine=(TextView) findViewById(R.id.endLine);
     }
 }
